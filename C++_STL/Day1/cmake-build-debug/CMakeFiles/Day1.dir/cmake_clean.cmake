cef3501abe0348d547cd4df33693ccfc82c68a65
LITturtlee <674588290@qq.com>
2021-03-28 20:36:27 +0800

    2021_03_28

---
+++ C++_STL/Day1/cmake-build-debug/CMakeFiles/Day1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day1.dir/main.cpp.o"
+  "Day1"
+  "Day1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
